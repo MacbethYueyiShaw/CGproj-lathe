feaa40e086613cb9e044469a76608d5e21dda80a
MikeShaw1 <1481796592@qq.com>
2021-01-04 23:31:19 +0800

    feat: material and shader

--- lathe/lathe.cpp
+++ lathe/lathe.cpp
@@ -26,6 +26,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
+void game_reset();
 //shader func: draw meshes
 void skybox_draw(Shader skyboxShader, unsigned int skyboxVAO, unsigned int cubemapTexture);
 void model_draw(Shader shader, Model mymodel, glm::vec3 position = glm::vec3(0.0f), glm::vec3 scale = glm::vec3(1.0f), glm::vec3 rotate_axe = glm::vec3(0.0f, 1.0f, 0.0f), float radians = 0.0f);
@@ -45,7 +46,7 @@ float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
 
 // lighting
-glm::vec3 lightPos(0.0f, 5.0f, -10.0f);
+glm::vec3 lightPos(0.0f, 2.0f, -2.0f);
 
 // timing
 float deltaTime = 0.0f;
@@ -53,8 +54,8 @@ float lastFrame = 0.0f;
 
 // cylinder data config
 //点阵精细度设置
-const int Y_SEGMENTS = 300;
-const int X_SEGMENTS = 20;
+const int Y_SEGMENTS = 200;
+const int X_SEGMENTS = 100;
 const int R_SEGMENTS = 100;
 //空间参数设置
 const GLfloat PI = 3.14159265358979323846f;
@@ -69,8 +70,29 @@ std::vector<float> cylinderVertices;//圆柱点集
 std::vector<float> cylinderAllData;//圆柱绘制数据集
 
 //切削刀具设置(刀用一个倒四棱锥表示)
+const glm::vec3 knife_pos_reset(-2.0f, 0.55f, 0.0f);
 glm::vec3 knife_pos = glm::vec3(-2.0f, 0.55f, 0.0f);//空间位置
 float knife_distance = 1.0f;
+
+//材质表 取自http://www.it.hiof.no/~borres/j3d/explain/light/p-materials.html
+//silver
+const glm::vec3 silver_ambient(0.23125f, 0.23125f, 0.23125f);
+const glm::vec3 silver_diffused(0.2775f, 0.2775f, 0.2775f);
+const glm::vec3 silver_specular(0.773911f, 0.773911f, 0.773911f);
+const float silver_shine = 89.6f;
+//log 木头这个的没找到，自己编了一个
+const glm::vec3 log_ambient(0.25f, 0.20725f, 0.20725f);
+const glm::vec3 log_diffused(0.9f, 0.729f, 0.729f);
+const glm::vec3 log_specular(0.296648f, 0.296648f, 0.296648f);
+const float log_shine = 11.264f;
+//tin
+const glm::vec3 tin_ambient( 0.105882f, 0.058824f, 0.113725f );
+const glm::vec3 tin_diffuse( 0.427451f, 0.470588f, 0.541176f );
+const glm::vec3 tin_specular( 0.333333f, 0.333333f, 0.521569f );
+float tin_shine =11.264f;
+
+//设置开关
+bool material_switch = 0; //0:wood , 1:silver
 
 ////////////////////////////////////////////////MAIN/////////////////////////////////////////////////
 int main()
@@ -117,7 +139,7 @@ int main()
     // configure global opengl state
     // -----------------------------
     // draw in wireframe
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     glEnable(GL_DEPTH_TEST);
 
 
@@ -345,12 +367,36 @@ int main()
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
         glm::mat4 model = glm::mat4(1.0f);
-  
+        // light properties
+        glm::vec3 lightColor = glm::vec3(1.0f);
+        glm::vec3 diffuseColor = lightColor * glm::vec3(1.0f); // decrease the influence
+        glm::vec3 ambientColor = diffuseColor * glm::vec3(1.0f); // low influence
+
         //draw lathe
         //model_draw(ourShader,ourModel, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.1f), glm::vec3(1.0f, 0.0f, 0.0f), -90.0f);
 
         //draw cylinder
         cylinderShader.use();
+        cylinderShader.setVec3("light.ambient", ambientColor);
+        cylinderShader.setVec3("light.diffuse", diffuseColor);
+        cylinderShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+        cylinderShader.setVec3("light.position", lightPos);
+        cylinderShader.setVec3("viewPos", camera.Position);
+        // material properties
+        if (material_switch)
+        {
+            cylinderShader.setVec3("material.ambient", silver_ambient);
+            cylinderShader.setVec3("material.diffuse", silver_diffused);
+            cylinderShader.setVec3("material.specular",silver_specular);
+            cylinderShader.setFloat("material.shininess", silver_shine);
+        }
+        else
+        {
+            cylinderShader.setVec3("material.ambient", log_ambient);
+            cylinderShader.setVec3("material.diffuse", log_diffused);
+            cylinderShader.setVec3("material.specular", log_specular);
+            cylinderShader.setFloat("material.shininess", log_shine);
+        }
         cylinderShader.setMat4("projection", projection);
         cylinderShader.setMat4("view", view);
         model = glm::mat4(1.0f);
@@ -368,21 +414,18 @@ int main()
         //glDrawElements(GL_TRIANGLES, X_SEGMENTS* Y_SEGMENTS * 6, GL_UNSIGNED_INT, 0);
 
         //draw knife
-        // light properties
         knifeShader.use();
-        glm::vec3 lightColor = glm::vec3(1.0f);
-        glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f); // decrease the influence
-        glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f); // low influence
         knifeShader.setVec3("light.ambient", ambientColor);
         knifeShader.setVec3("light.diffuse", diffuseColor);
         knifeShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
         knifeShader.setVec3("light.position", lightPos);
+        knifeShader.setVec3("viewPos", camera.Position);
 
         // material properties
-        knifeShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
-        knifeShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
-        knifeShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f); // specular lighting doesn't have full effect on this object's material
-        knifeShader.setFloat("material.shininess", 32.0f);
+        knifeShader.setVec3("material.ambient", tin_ambient);
+        knifeShader.setVec3("material.diffuse", tin_diffuse);
+        knifeShader.setVec3("material.specular", tin_specular);
+        knifeShader.setFloat("material.shininess", tin_shine);
 
         // view/projection transformations
         knifeShader.setMat4("projection", projection);
@@ -437,16 +480,21 @@ int main()
 // ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow* window)
 {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
         glfwSetWindowShouldClose(window, true);
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    }
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){
         camera.ProcessKeyboard(FORWARD, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
         camera.ProcessKeyboard(BACKWARD, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
         camera.ProcessKeyboard(LEFT, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
         camera.ProcessKeyboard(RIGHT, deltaTime);
+    }
     if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
         knife_pos.y = knife_pos.y + 0.5f / R_SEGMENTS;
         knife_distance += 1.0f / R_SEGMENTS;
@@ -489,6 +537,26 @@ void processInput(GLFWwindow* window)
             }
         }
     }
+    if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) {
+        material_switch = 1;
+        return;
+    }
+    if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) {
+        material_switch = 0;
+        return;
+    }
+    if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) {
+        game_reset();
+        return;
+    }
+}
+//reset game
+void game_reset()
+{
+    knife_distance = 1.0f;
+    knife_pos = knife_pos_reset;
+    cylinder_radius_vector_init();
+    cylinder_data_update();
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -719,25 +787,49 @@ void cylinder_data_update()
             cylinderAllData.push_back(y1);
             cylinderAllData.push_back(z1);
 
+            cylinderAllData.push_back(normal.x);
+            cylinderAllData.push_back(normal.y);
+            cylinderAllData.push_back(normal.z);
+
             cylinderAllData.push_back(x2);
             cylinderAllData.push_back(y2);
             cylinderAllData.push_back(z2);
+
+            cylinderAllData.push_back(normal.x);
+            cylinderAllData.push_back(normal.y);
+            cylinderAllData.push_back(normal.z);
 
             cylinderAllData.push_back(x3);
             cylinderAllData.push_back(y3);
             cylinderAllData.push_back(z3);
 
+            cylinderAllData.push_back(normal.x);
+            cylinderAllData.push_back(normal.y);
+            cylinderAllData.push_back(normal.z);
+
             cylinderAllData.push_back(x4);
             cylinderAllData.push_back(y4);
             cylinderAllData.push_back(z4);
+
+            cylinderAllData.push_back(normal.x);
+            cylinderAllData.push_back(normal.y);
+            cylinderAllData.push_back(normal.z);
 
             cylinderAllData.push_back(x5);
             cylinderAllData.push_back(y5);
             cylinderAllData.push_back(z5);
 
+            cylinderAllData.push_back(normal.x);
+            cylinderAllData.push_back(normal.y);
+            cylinderAllData.push_back(normal.z);
+
             cylinderAllData.push_back(x6);
             cylinderAllData.push_back(y6);
             cylinderAllData.push_back(z6);
+        
+            cylinderAllData.push_back(normal.x);
+            cylinderAllData.push_back(normal.y);
+            cylinderAllData.push_back(normal.z);
         }
     }
 }
@@ -756,8 +848,10 @@ void cylinder_buffer_update(unsigned int cylinderVAO, unsigned int cylinderVBO)
     //glBufferData(GL_ELEMENT_ARRAY_BUFFER, cylinderIndices.size() * sizeof(int), &cylinderIndices[0], GL_STATIC_DRAW);
 
     //设置顶点属性指针
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     //解绑VAO和VBO <- 没必要
     //glBindBuffer(GL_ARRAY_BUFFER, 0);
